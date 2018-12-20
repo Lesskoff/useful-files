33254227bf995fc20c7cd1c52970f798573b734e
Gleb L <iutas@bk.ru>
2018-12-20 11:02:10 +0300

    Update seo-redirects.php

--- seo-redirects.php
+++ seo-redirects.php
@@ -1,11 +1,11 @@
 <?php
-$links = array(
+$links = [
 	'/from' => '/to',
-	);
+];
 
 
-if(isset($links[$_SERVER['REQUEST_URI']])){
-	$redirect = 'http://' . $_SERVER['HTTP_HOST'] . $links[$_SERVER['REQUEST_URI']];
-	header('HTTP/1.1 301 Moved Permanently');
-	header('Location: ' . $redirect);
+if(isset($links[$_SERVER["REQUEST_URI"]])){
+	$redirect = "http://" . $_SERVER["HTTP_HOST"] . $links[$_SERVER["REQUEST_URI"]];
+  header("Location: " . $redirect, true,  301); exit;
 }
+?>
