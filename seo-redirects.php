cdbe00735816676b981012cf99219e541a778a0e
Gleb L <iutas@bk.ru>
2018-12-20 11:11:12 +0300

    Update seo-redirects.php

--- seo-redirects.php
+++ seo-redirects.php
@@ -6,6 +6,6 @@ $links = [
 
 if(isset($links[$_SERVER["REQUEST_URI"]])){
 	$redirect = "http://" . $_SERVER["HTTP_HOST"] . $links[$_SERVER["REQUEST_URI"]];
-  header("Location: " . $redirect, true,  301); exit;
+	header("Location: " . $redirect, true,  301); exit;
 }
 ?>
