64341c921bc6ede63877c0f72a64d22524f23faa
Gleb L <iutas@bk.ru>
2019-03-11 17:30:36 +0300

    Update mail.php

--- Form with reCaptcha/mail.php
+++ Form with reCaptcha/mail.php
@@ -1,201 +1,45 @@
 <?php
-  // открываем сессию
-  session_start();
 
-  // переменная в которую будем сохранять результат работы
-  $data['result']='error';
+$msg = array(
+	'noRecaptchaError' => false,
+	'noErrors' => false,
+);
 
-  // разрешённые типы файлов
-  $allowedExtension = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
+if (isset($_POST['g-recaptcha-response']))
+{
 
-  // директория для хранения файлов ()
-  $pathToFile = $_SERVER['DOCUMENT_ROOT'].'/feedback/files/';
+	$url = 'https://www.google.com/recaptcha/api/siteverify';
+	$params = array(
+		'secret' => "SECRET_SITEKEY,
+		'response' => $_POST['g-recaptcha-response'],
+		'remoteip' => $_SERVER['REMOTE_ADDR']
+	);
+	
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	
+	$response = curl_exec($ch);
+	if(!empty($response))
+		$decoded_response = json_decode($response);
+	
+	// $success = false;
+	
+	if ($decoded_response && $decoded_response->success)
+		$success = $decoded_response->success;
+	
+	if ($success)
+		$msg['noRecaptchaError'] = true;
+}
 
-  // максимальный размер файла
-  $maxSizeFile = 524288;
+if(isset($_POST['name']) && !empty($_POST['name']) && isset($_POST['phone']) && !empty($_POST['phone']))
+	$msg['noErrors'] = true;
 
-  // ваш секретный ключ
-  $secret = '6NepjAsGBBABBN7_Qy9yfzShcKmc70X2kXQyX1WO';
+if($msg['noErrors'] && $msg['noRecaptchaError']) {
+	// Здесь можно написать скрипт отправки самой заявки, например, при помощи функции mail()
+}
 
-  // функция для проверки длины строки
-  function validStringLength($string,$min,$max) {
-    $length = mb_strlen($string,'UTF-8');
-    if (($length<$min) || ($length>$max)) {
-      return false;
-    }
-    else {
-      return true;
-    }
-  }
-
-  // если данные были отправлены методом POST, то...
-  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-    $data['result']='success';
-
-    //получить имя, которое ввёл пользователь
-    if (isset($_POST['name'])) {
-      $name = $_POST['name'];
-      if (!validStringLength($name,2,30)) {
-        $data['name']='Поля имя содержит недопустимое количество символов.';
-        $data['result']='error';
-      }
-    } else {
-      $data['result']='error';
-    }
-    //получить email, которое ввёл пользователь
-    if (isset($_POST['email'])) {
-      $email = $_POST['email'];
-      if (!filter_var($email,FILTER_VALIDATE_EMAIL)) {
-        $data['email']='Поле email введено неправильно';
-        $data['result']='error';
-      }
-    } else {
-      $data['result']='error';
-    }
-    //получить сообщение, которое ввёл пользователь
-    if (isset($_POST['message'])) {
-      $message = $_POST['message'];
-      if (!validStringLength($message,20,500)) {
-        $data['message']='Поле сообщение содержит недопустимое количество символов.';
-        $data['result']='error';
-      }
-    } else {
-      $data['result']='error';
-    }
-
-    // если не существует ни одной ошибки, то прододжаем...
-    if ($data['result']=='success') {
-      // однократное включение файла autoload.php (клиентская библиотека reCAPTCHA PHP)
-      require_once (dirname(__FILE__).'/recaptcha/autoload.php');
-      // если в массиве $_POST существует ключ g-recaptcha-response, то...
-      if (isset($_POST['g-recaptcha-response'])) {
-        // создать экземпляр службы recaptcha, используя секретный ключ
-        $recaptcha = new \ReCaptcha\ReCaptcha($secret);
-        // получить результат проверки кода recaptcha
-        $resp = $recaptcha->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
-        // если результат положительный, то...
-        if ($resp->isSuccess()){
-          // действия, если код captcha прошёл проверку
-          //обработаем файлы, загруженные пользователем посредством элементов с name="images[]"
-          // если ассоциатианый массив $_FILES["images"] существует, то
-          if(isset($_FILES["images"])) {
-            // переберём все файлы (изображения)
-            $files = array();
-            foreach ($_FILES["images"]["error"] as $key => $error) {
-              // если ошибок не возникло, т.е. файл был успешно загружен на сервер, то...
-              if ($error == UPLOAD_ERR_OK) {
-                // имя файла на устройстве пользователя
-                $nameFile = $_FILES['images']['name'][$key];
-                // расширение загруженного пользователем файла в нижнем регистре
-                $extFile = mb_strtolower(pathinfo($nameFile, PATHINFO_EXTENSION));
-                // размер файла
-                $sizefile = $_FILES['images']['size'][$key];
-                //myme-тип файла
-                $filetype = $_FILES['images']['type'][$key];
-                // проверить расширение файла, размер файла и mime-тип
-                if (!array_key_exists($extFile, $allowedExtension)) {
-                  $data['files']='Ошибка при загрузке файлов (неверное расширение).';
-                  $data['result']='error';
-                } elseif ($sizefile > $maxSizeFile) {
-                  $data['files']='Ошибка при загрузке файлов (размер превышает 512Кбайт).';
-                  $data['result']='error';
-                } elseif (!in_array($filetype, $allowedExtension)){
-                  $data['files']='Ошибка при загрузке файлов (неверный тип файла).';
-                  $data['result']='error';
-                } else {
-                  //ошибок не возникло, продолжаем...
-                  // временное имя, с которым принятый файл был сохранён на сервере
-                  $tmpFile = $_FILES['images']['tmp_name'][$key];
-                  // уникальное имя файла
-                  $newFileName = uniqid('img_', true).'.'.$extFile;
-                  // полное имя файла
-                  $newFullFileName = $pathToFile.$newFileName;
-                  // перемещаем файл в директорию
-                  if (!move_uploaded_file($tmpFile, $newFullFileName)) {
-                    // ошибка при перемещении файла
-                    $data['files']='Ошибка при загрузке файлов.';
-                    $data['result']='error';
-                  } else {
-                    $files[] = $newFullFileName;
-                  }
-                }
-              } else {
-                //ошибка при загрузке файл на сервер
-                $data['result']='error';
-              }
-            }
-          }
-        } else {
-          // иначе передать ошибку
-          $errors = $resp->getErrorCodes();
-          $data['error-captcha']=$errors;
-          $data['msg']='Код капчи не прошёл проверку на сервере';
-          $data['result']='error';
-        }
-      } else {
-        $data['result']='error';
-      }
-    }
-  } else {
-    //ошибка, не существует ассоциативный массив $_POST["send-message"]
-    $data['result']='error';
-  }
-
-  // дальнейшие действия (ошибок не обнаружено)
-  if ($data['result']=='success') {
-    //1. Сохрание формы в файл
-    $output = "---------------------------------" . "\n";
-    $output .= date("d-m-Y H:i:s") . "\n";
-    $output .= "Имя пользователя: " . $name . "\n";
-    $output .= "Адрес email: " . $email . "\n";
-    $output .= "Сообщение: " . $message . "\n";
-    if (isset($files)) {
-      $output .= "Файлы: " . "\n";
-      foreach ($files as $value) {
-         $output .= $value . "\n";
-      }
-    }
-    if (file_put_contents(dirname(__FILE__).'/message.txt', $output, FILE_APPEND | LOCK_EX)) {
-      $data['result']='success';
-    } else {
-      $data['result']='error';
-    }
-
-    //2. Отправляем на почту
-    // включить файл PHPMailerAutoload.php
-    require_once dirname(__FILE__) . '/phpmailer/PHPMailerAutoload.php';
-    //формируем тело письма
-    $output = "Дата: " . date("d-m-Y H:i") . "\n";
-    $output .= "Имя пользователя: " . $name . "\n";
-    $output .= "Адрес email: " . $email . "\n";
-    $output .= "Сообщение: " . "\n" . $message . "\n";
-
-    // создаём экземпляр класса PHPMailer
-    $mail = new PHPMailer;
-
-    $mail->CharSet = 'UTF-8';
-    $mail->From      = 'email@mysite.ru';
-    $mail->FromName  = 'Имя сайта';
-    $mail->Subject   = 'Сообщение с формы обратной связи';
-    $mail->Body      = $output;
-    $mail->AddAddress( 'myemail@mail.ru' );
-    // прикрепляем файлы к письму
-    if (isset($files)) {
-      foreach ($files as $value) {
-        $mail->addAttachment($value);
-      }
-    }
-
-    // отправляем письмо
-    if ($mail->Send()) {
-      $data['result']='success';
-    } else {
-      $data['result']='error';
-    }
-
-  }
-
-  echo json_encode($data);
-
-?>
+echo json_encode($msg);
